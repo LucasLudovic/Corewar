22652e75dc8d3575d23a2f3f75c3cd8dd500414f
Valiance <lucas.ludovic@epitech.eu>
2024-04-30 10:47:32 +0200

    [ADDING FILE] Adding prototype of the live instruction

---
+++ src/opcodes/opcode_0x01.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** Corewar
+** File description:
+** Indicate that the player is alive
+*/
+
+#include <stddef.h>
+#include "my_macros.h"
+
+int execute_opcode_live(champion_t *champion)
+{
+    if (champion == NULL)
+        return FAILURE;
+
+}
