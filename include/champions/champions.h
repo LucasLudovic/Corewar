9fb01170e1f4954f70268819ad814835aa58ba37
Valiance <lucas.ludovic@epitech.eu>
2024-04-30 19:30:50 +0200

    [UPDATE] Update the register with the parameters retrieved

--- include/champions/champions.h
+++ include/champions/champions.h
@@ -16,7 +16,7 @@ typedef struct champions {
     char *name;
     FILE *file_stream;
     int nbr_cycles;
-    uint32_t registers[REG_SIZE];
+    uint32_t registers[REG_NUMBER];
     uint64_t program_counter;
     int load_address;
     int play_number;
--- src/opcodes/opcode_0x02.c
+++ src/opcodes/opcode_0x02.c
@@ -8,16 +8,41 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "champions/champions.h"
+#include "op.h"
 #include "virtualmachine/initialize_vm.h"
 #include "corewar.h"
 #include "my.h"
 #include "my_macros.h"
 
-int execute_opcode_ld(cpu_t *cpu, champions_t *champion)
+static
+int update_register(cpu_t *cpu, champions_t *champion, uint8_t coding_byte)
 {
-    int coding_byte = 0;
     uint32_t first_param = 0;
     uint8_t second_param = 0;
+
+    if (coding_byte >> 6 & T_DIR)
+        if (fread(&first_param, DIR_SIZE, 1, champion->file_stream) <= 0)
+            return display_error("Unable to get the first DIR param for ld\n");
+    if (coding_byte >> 6 & T_IND) {
+        if (fread(&first_param, IND_SIZE, 1, champion->file_stream) <= 0)
+            return display_error("Unable to get the first IND param for ld\n");
+        if (first_param >= MEM_SIZE)
+            return display_error("First param too big for ld\n");
+        first_param = *((uint32_t *)(&(cpu->memory[first_param
+            + champion->program_counter])));
+    }
+    if (fread(&second_param, REG_SIZE, 1, champion->file_stream) <= 0
+        || second_param >= REG_NUMBER)
+        return display_error("Second param not valid for ld\n");
+    champion->registers[second_param] = champion->program_counter;
+    champion->registers[second_param] += (first_param % IDX_MOD);
+    return SUCCESS;
+}
+
+int execute_opcode_ld(cpu_t *cpu, champions_t *champion)
+{
+    uint8_t coding_byte = 0;
 
     if (cpu == NULL || champion == NULL)
         return display_error("Unable to retrieve structs for ld\n");
@@ -25,14 +50,10 @@ int execute_opcode_ld(cpu_t *cpu, champions_t *champion)
         return display_error("Unable to access champion file\n");
     if (fread(&coding_byte, sizeof(uint8_t), 1, champion->file_stream) <= 0)
         return display_error("Unable to retrieve coding byte in opcode ld\n");
-    if ((coding_byte >> 6 & T_DIR || coding_byte >> 6 & T_IND) == FALSE)
+    if (((coding_byte >> 6 & T_DIR || coding_byte >> 6 & T_IND)
+        && (coding_byte << 2 >> 6 & T_REG)) == FALSE)
         return display_error("Wrong type for the first parameter\n");
-    if (coding_byte >> 6 & T_DIR) {
-        if (fread(&first_param, sizeof(int), 1, champion->file_stream) <= 0)
-            return display_error("Unable to get the first param for ld\n");
-        champion->registers[second_param] = first_param;
-    }
-    if (coding_byte >> 6 & T_IND)
-        champion->registers[second_param] = cpu->memory[first_param];
+    if (update_register(cpu, champion, coding_byte) != SUCCESS)
+        return FAILURE;
     return SUCCESS;
 }
