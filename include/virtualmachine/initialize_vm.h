e240ac444ba7e2329e7309166894318d5bba07b9
Valiance <lucas.ludovic@epitech.eu>
2024-05-07 14:18:37 +0200

    [UPDATE] Loop through the champions to retrieve the functions

--- include/virtualmachine/initialize_vm.h
+++ include/virtualmachine/initialize_vm.h
@@ -21,7 +21,7 @@ typedef enum state {
 
 typedef struct cpu_s {
     uint8_t memory[MEM_SIZE];
-    champions_t champions[NB_CHAMPIONS];
+    champions_t *champions[NB_CHAMPIONS];
     cpu_state_t state;
 } cpu_t;
 
---
+++ src/virtualmachine/virtual_machine.c
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2024
+** 42sh
+** File description:
+** Execute the virtual machine
+*/
+
+#include "champions/champions.h"
+#include "virtualmachine/initialize_vm.h"
+#include "my_macros.h"
+#include "my.h"
+
+static
+int retrieve_champions_header(cpu_t *cpu)
+{
+    return SUCCESS;
+}
+
+static
+int execute_single_champion(cpu_t *cpu, champions_t *champion)
+{
+    return SUCCESS;
+}
+
+static
+int execute_champions(cpu_t *cpu)
+{
+    for (size_t i = 0; cpu->champions[i] != NULL && i < NB_CHAMPIONS; i += 1) {
+        if (execute_single_champion(cpu, cpu->champions[i]) == FAILURE)
+            return FAILURE;
+    }
+    return SUCCESS;
+}
+
+int execute_virtual_machine(cpu_t *cpu)
+{
+    if (cpu == NULL)
+        return display_error("Unable to access cpu informations\n");
+    if (retrieve_champions_header(cpu) == FAILURE)
+        return FAILURE;
+    while (cpu->state != CPU_HALTED) {
+        if (execute_champions(cpu) == FAILURE)
+            return FAILURE;
+    }
+    return SUCCESS;
+}
