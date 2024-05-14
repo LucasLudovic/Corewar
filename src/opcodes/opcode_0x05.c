23d3ca91071188f647505317d2b8239b5ee57800
Valiance <lucas.ludovic@epitech.eu>
2024-05-15 00:01:28 +0200

    [UPDATE] Update the program counter for opcode add and sub

--- src/opcodes/opcode_0x05.c
+++ src/opcodes/opcode_0x05.c
@@ -23,12 +23,14 @@ int execute_opcode_sub(cpu_t *cpu, champions_t *champion)
         return display_error("Unable to retrieve structs for add\n");
     current_counter = (champion->program_counter + CODING_BYTE + 1) % MEM_SIZE;
     first_parameter = cpu->memory[current_counter];
-    current_counter += (current_counter + 1) % MEM_SIZE;
+    current_counter = (current_counter + 1) % MEM_SIZE;
     second_parameter = cpu->memory[current_counter];
-    current_counter += (current_counter + 1) % MEM_SIZE;
+    current_counter = (current_counter + 1) % MEM_SIZE;
     third_parameter = cpu->memory[current_counter];
     registers[third_parameter] =
         registers[first_parameter] - registers[second_parameter];
     champion->carry = !champion->carry;
+    champion->program_counter += CODING_BYTE + 3 + 1;
+    champion->program_counter %= MEM_SIZE;
     return SUCCESS;
 }
