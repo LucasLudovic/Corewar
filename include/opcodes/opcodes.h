53a67d5e637f1dfea764c81e8b2c9491f4167d46
Valiance <lucas.ludovic@epitech.eu>
2024-05-15 01:30:52 +0200

    [FEATURE] Adding the instructions for opcode sti

--- include/opcodes/opcodes.h
+++ include/opcodes/opcodes.h
@@ -13,6 +13,10 @@ extern int (*instruction[])(cpu_t *cpu, champions_t *champion);
 
 int retrieve_first_parameter(cpu_t *cpu, champions_t *champion,
     int *first_parameter, int byte_read);
+int retrieve_second_parameter(cpu_t *cpu, champions_t *champion,
+    int *first_parameter, int byte_read);
+int retrieve_third_parameter(cpu_t *cpu, champions_t *champion,
+    int *first_parameter, int byte_read);
 int execute_opcode_live(cpu_t *cpu, champions_t *champion);
 int execute_opcode_ld(cpu_t *cpu, champions_t *champion);
 int execute_opcode_st(cpu_t *cpu, champions_t *champion);
--- src/opcodes/opcode_0x0b.c
+++ src/opcodes/opcode_0x0b.c
@@ -18,7 +18,9 @@ int execute_opcode_sti(cpu_t *cpu, champions_t *champion)
     int first_param = 0;
     int second_param = 0;
     int third_param = 0;
-    int byte_read = 0;
+    int byte_read = 1;
+    int previous_byte_read = 0;
+    int address_to_modify = 0;
 
     if (cpu == NULL || champion == NULL)
         return display_error("Unable to retrieve structs for sti\n");
@@ -26,7 +28,18 @@ int execute_opcode_sti(cpu_t *cpu, champions_t *champion)
         byte_read);
     byte_read = retrieve_second_parameter(cpu, champion,
         &second_param, byte_read);
+    if (byte_read == 3)
+        address_to_modify += champion->registers[second_param];
+    else
+        address_to_modify += second_param;
+    previous_byte_read = byte_read;
     byte_read = retrieve_third_parameter(cpu, champion,
         &third_param, byte_read);
+    if (byte_read == previous_byte_read + 1)
+        address_to_modify += champion->registers[third_param];
+    else
+        address_to_modify += third_param;
+    cpu->memory[(champion->program_counter + (address_to_modify % IDX_MOD))
+        % MEM_SIZE] = champion->registers[first_param];
     return SUCCESS;
 }
