f97cd24239a898eebfcc9b25d6bfc45ff18eba09
Valiance <lucas.ludovic@epitech.eu>
2024-05-14 15:27:02 +0200

    [FIX] Use += not to lose the previous bits in opcode st

--- src/opcodes/opcode_0x03.c
+++ src/opcodes/opcode_0x03.c
@@ -26,11 +26,11 @@ void store_value(cpu_t *cpu, champions_t *champion, uint8_t coding_byte)
     else {
         second_param = cpu->memory[(champion->program_counter + 3) % MEM_SIZE];
         second_param = second_param << 2;
-        second_param = cpu->memory[(champion->program_counter + 4) % MEM_SIZE];
+        second_param += cpu->memory[(champion->program_counter + 4) % MEM_SIZE];
         second_param = second_param << 2;
-        second_param = cpu->memory[(champion->program_counter + 5) % MEM_SIZE];
+        second_param += cpu->memory[(champion->program_counter + 5) % MEM_SIZE];
         second_param = second_param << 2;
-        second_param = cpu->memory[(champion->program_counter + 6) % MEM_SIZE];
+        second_param += cpu->memory[(champion->program_counter + 6) % MEM_SIZE];
         cpu->memory[(champion->program_counter + second_param) % IDX_MOD] = champion->registers[first_param]; 
     }
 
