899119174473ddd2c7a4eb9b8f89301b3fb877b2
Yuuko <tilio.huart@epitech.eu>
2024-05-15 13:42:03 +0200

    [UPDATE] retrieve params in opcode_0x06

--- src/opcodes/opcode_0x06.c
+++ src/opcodes/opcode_0x06.c
@@ -9,235 +9,76 @@
 #include "my.h"
 #include "op.h"
 #include "virtualmachine/initialize_vm.h"
+#include "opcodes/opcodes.h"
 #include <stdint.h>
 
 static
-void make_operation_with_register(cpu_t *cpu, champions_t *champion)
+void first_param_register(champions_t *champion, size_t param[3], int bytes[3])
 {
-    uint8_t first_param = 0;
-    uint8_t second_param = 0;
-    uint8_t third_param = 0;
-
-    first_param = cpu->memory[(champion->program_counter + CODING_BYTE + 1)
-        % MEM_SIZE];
-    second_param = cpu->memory[(champion->program_counter +
-        CODING_BYTE + 2) % MEM_SIZE];
-    third_param = cpu->memory[(champion->program_counter +
-        CODING_BYTE + 3) % MEM_SIZE];
-    champion->registers[third_param] = champion->registers[first_param] &
-        champion->registers[second_param];
+    if (bytes[1] == 2 && bytes[2] == 3)
+        champion->registers[param[2]] = champion->registers[param[0]] &
+            champion->registers[param[1]];
+    if (bytes[1] == 2 && bytes[2] == 4)
+        champion->registers[param[2]] = champion->registers[param[0]] &
+            param[1];
+    if (bytes[1] == 2 && bytes[2] == 6)
+        champion->registers[param[2]] = champion->registers[param[0]] &
+            param[2];
 }
 
 static
-void make_operation_with_register_and_direct(cpu_t *cpu, champions_t *champion)
+void first_param_direct(champions_t *champion, size_t param[3], int bytes[3])
 {
-    uint8_t first_param = 0;
-    uint16_t second_param = 0;
-    uint8_t third_param = 0;
-
-    first_param = cpu->memory[(champion->program_counter + CODING_BYTE + 1)
-        % MEM_SIZE];
-    second_param = cpu->memory[(champion->program_counter + CODING_BYTE + 2)
-        % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + CODING_BYTE + 3)
-        % MEM_SIZE];
-    third_param = cpu->memory[(champion->program_counter + CODING_BYTE + 4)
-        % MEM_SIZE];
-    champion->registers[third_param] = champion->registers[first_param]
-        & second_param;
+    if (bytes[1] == 3 && bytes[2] == 4)
+        champion->registers[param[2]] = param[0] &
+            champion->registers[param[1]];
+    if (bytes[1] == 3 && bytes[2] == 5)
+        champion->registers[param[2]] = champion->registers[param[0]] &
+            param[1];
+    if (bytes[1] == 3 && bytes[2] == 7)
+        champion->registers[param[2]] = param[0] & param[2];
 }
 
 static
-void make_operation_with_register_and_indirect(cpu_t *cpu,
-    champions_t *champion)
+void first_param_indirect(champions_t *champion, size_t param[3], int bytes[3])
 {
-    uint8_t first_param = 0;
-    uint32_t second_param = 0;
-    uint8_t third_param = 0;
-
-    first_param = cpu->memory[(champion->program_counter + 2) % MEM_SIZE];
-    second_param = cpu->memory[(champion->program_counter + CODING_BYTE + 2)
-        % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + CODING_BYTE + 3)
-        % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + CODING_BYTE + 4)
-        % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + CODING_BYTE + 5)
-        % MEM_SIZE];
-    third_param = cpu->memory[(champion->program_counter + CODING_BYTE + 6)
-        % MEM_SIZE];
-    champion->registers[third_param] = champion->registers[first_param]
-        & second_param;
+    if (bytes[1] == 5 && bytes[2] == 6)
+        champion->registers[param[2]] = param[0] &
+            champion->registers[param[1]];
+    if (bytes[1] == 5 && bytes[2] == 7)
+        champion->registers[param[2]] = champion->registers[param[0]] &
+            param[1];
+    if (bytes[1] == 5 && bytes[2] == 9)
+        champion->registers[param[2]] = param[0] & param[2];
 }
 
 static
-void make_operation_with_direct_and_register(cpu_t *cpu, champions_t *champion)
+void make_operation(champions_t *champion, size_t param[3], int bytes[3])
 {
-    uint8_t first_param = 0;
-    uint32_t second_param = 0;
-    uint8_t third_param = 0;
-
-    first_param = cpu->memory[(champion->program_counter + CODING_BYTE + 1)
-        % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + CODING_BYTE + 2)
-        % MEM_SIZE];
-    second_param = cpu->memory[(champion->program_counter + CODING_BYTE + 3)
-        % MEM_SIZE];
-    third_param = cpu->memory[(champion->program_counter + CODING_BYTE + 4)
-        % MEM_SIZE];
-    champion->registers[third_param] = first_param
-        & champion->registers[second_param];
+    first_param_register(champion, param, bytes);
+    first_param_direct(champion, param, bytes);
+    first_param_indirect(champion, param, bytes);
 }
 
 static
-void make_operation_with_direct_and_direct(cpu_t *cpu, champions_t *champion)
+void retrieve_param(cpu_t *cpu, champions_t *champion)
 {
-    uint8_t first_param = 0;
-    uint32_t second_param = 0;
-    uint8_t third_param = 0;
+    size_t param[3] = {0, 0, 0};
+    int bytes[3] = {1, 0, 0};
 
-    first_param = cpu->memory[(champion->program_counter + CODING_BYTE + 1)
-        % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + CODING_BYTE + 2)
-        % MEM_SIZE];
-    second_param = cpu->memory[(champion->program_counter + CODING_BYTE + 3)
-        % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + CODING_BYTE + 4)
-        % MEM_SIZE];
-    third_param = cpu->memory[(champion->program_counter + CODING_BYTE + 5)
-        % MEM_SIZE];
-    champion->registers[third_param] = first_param & second_param;
-}
-
-static
-void make_operation_with_direct_and_indirect(cpu_t *cpu, champions_t *champion)
-{
-    uint8_t first_param = 0;
-    uint32_t second_param = 0;
-    uint8_t third_param = 0;
-
-    first_param = cpu->memory[(champion->program_counter + 2) % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + 3) % MEM_SIZE];
-    second_param = cpu->memory[(champion->program_counter + 4) % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + 5) % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + 6) % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + 7) % MEM_SIZE];
-    third_param = cpu->memory[(champion->program_counter + 8) % MEM_SIZE];
-    champion->registers[third_param] = first_param & second_param;
-}
-
-static
-void make_operation_with_indirect_and_register(cpu_t *cpu,
-    champions_t *champion)
-{
-    uint8_t first_param = 0;
-    uint32_t second_param = 0;
-    uint8_t third_param = 0;
-
-    first_param = cpu->memory[(champion->program_counter + 2) % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + 3) % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + 4) % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + 5) % MEM_SIZE];
-    second_param = cpu->memory[(champion->program_counter + 6) % MEM_SIZE];
-    third_param = cpu->memory[(champion->program_counter + 7) % MEM_SIZE];
-    champion->registers[third_param] = first_param &
-        champion->registers[second_param];
-}
-
-static
-void make_operation_with_indirect_and_direct(cpu_t *cpu,
-    champions_t *champion)
-{
-    uint8_t first_param = 0;
-    uint32_t second_param = 0;
-    uint8_t third_param = 0;
-
-    first_param = cpu->memory[(champion->program_counter + 2) % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + 3) % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + 4) % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + 5) % MEM_SIZE];
-    second_param = cpu->memory[(champion->program_counter + 6) % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + 7) % MEM_SIZE];
-    third_param = cpu->memory[(champion->program_counter + 8) % MEM_SIZE];
-    champion->registers[third_param] = first_param
-        & champion->registers[second_param];
-}
-
-static
-void make_operation_with_indirect_and_indirect(cpu_t *cpu,
-    champions_t *champion)
-{
-    uint8_t first_param = 0;
-    uint32_t second_param = 0;
-    uint8_t third_param = 0;
-
-    first_param = cpu->memory[(champion->program_counter + 2) % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + 3) % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + 4) % MEM_SIZE];
-    first_param <<= 2;
-    first_param += cpu->memory[(champion->program_counter + 5) % MEM_SIZE];
-    second_param = cpu->memory[(champion->program_counter + 6) % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + 7) % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + 8) % MEM_SIZE];
-    second_param <<= 2;
-    second_param += cpu->memory[(champion->program_counter + 9) % MEM_SIZE];
-    third_param = cpu->memory[(champion->program_counter + 10) % MEM_SIZE];
-    champion->registers[third_param] = first_param
-        & champion->registers[second_param];
-}
-
-static
-void make_operation(cpu_t *cpu, champions_t *champion, int coding_byte)
-{
-    if ((coding_byte >> 6 & T_REG) && ((coding_byte << 2) >> 6 & T_REG))
-        make_operation_with_register(cpu, champion);
-    if ((coding_byte >> 6 & T_REG) && ((coding_byte << 2) >> 6) & T_DIR)
-        make_operation_with_register_and_direct(cpu, champion);
-    if ((coding_byte >> 6 & T_REG) && ((coding_byte << 2) >> 6) & T_IND)
-        make_operation_with_register_and_indirect(cpu, champion);
-    if ((coding_byte >> 6 & T_DIR) && ((coding_byte << 2) >> 6) & T_REG)
-        make_operation_with_direct_and_register(cpu, champion);
-    if ((coding_byte >> 6 & T_DIR) && ((coding_byte << 2) >> 6) & T_DIR)
-        make_operation_with_direct_and_direct(cpu, champion);
-    if ((coding_byte >> 6 & T_DIR) && ((coding_byte << 2) >> 6) & T_IND)
-        make_operation_with_direct_and_indirect(cpu, champion);
-    if ((coding_byte >> 6 & T_IND) && ((coding_byte << 2) >> 6) & T_REG)
-        make_operation_with_indirect_and_register(cpu, champion);
-    if ((coding_byte >> 6 & T_IND) && ((coding_byte << 2) >> 6) & T_DIR)
-        make_operation_with_indirect_and_direct(cpu, champion);
-    if ((coding_byte >> 6 & T_IND) && ((coding_byte << 2) >> 6) & T_DIR)
-        make_operation_with_indirect_and_indirect(cpu, champion);
+    bytes[0] = retrieve_first_parameter(cpu, champion, &param[0], bytes[0]);
+    bytes[1] = bytes[0];
+    bytes[0] = retrieve_second_parameter(cpu, champion, &param[1], bytes[0]);
+    bytes[2] = bytes[0];
+    bytes[0] = retrieve_third_parameter(cpu, champion, &param[2], bytes[0]);
+    make_operation(champion, param, bytes);
 }
 
 int execute_opcode_and(cpu_t *cpu, champions_t *champion)
 {
-    int coding_byte = cpu->memory[champion->program_counter + 1];
-
     if (cpu == NULL || champion == NULL)
         return display_error("Unable to retrieve structs for st\n");
     champion->index = false;
-    make_operation(cpu, champion, coding_byte);
+    retrieve_param(cpu, champion);
     return SUCCESS;
 }
