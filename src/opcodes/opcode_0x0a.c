b6e98ece906bd8c348b1e03f9784d686d3e16840
Valiance <lucas.ludovic@epitech.eu>
2024-05-15 15:14:08 +0200

    [CODING STYLE] Remove majors in opcodes ldi

--- src/opcodes/opcode_0x0a.c
+++ src/opcodes/opcode_0x0a.c
@@ -13,23 +13,37 @@
 #include <stddef.h>
 
 static
-int retrieve_sum(cpu_t *cpu, champions_t *champion, int *bytes, size_t sum)
+int update_sum_first(cpu_t *cpu, champions_t *champion, int *bytes,
+    bool *first_register)
 {
     size_t first_param = 0;
-    size_t second_param = 0;
-    bool first_register = false;
+    size_t sum = 0;
 
     *bytes = retrieve_first_parameter(cpu, champion, &first_param, *bytes);
     if (*bytes == 2) {
-        first_register = true;
-        sum = cpu->memory[(champion->program_counter + (champion->registers[first_param] % IDX_MOD)) % MEM_SIZE];
+        *first_register = true;
+        sum = cpu->memory[(champion->program_counter +
+            (champion->registers[first_param] % IDX_MOD)) % MEM_SIZE];
         sum <<= 8;
-        sum += cpu->memory[(champion->program_counter + (champion->registers[first_param] % IDX_MOD) + 1) % MEM_SIZE];
+        sum += cpu->memory[(champion->program_counter +
+            (champion->registers[first_param] % IDX_MOD) + 1) % MEM_SIZE];
     } else {
-        sum = cpu->memory[(champion->program_counter + (first_param % IDX_MOD)) % MEM_SIZE];
+        sum = cpu->memory[(champion->program_counter +
+            (first_param % IDX_MOD)) % MEM_SIZE];
         sum <<= 8;
-        sum += cpu->memory[(champion->program_counter + (first_param % IDX_MOD) + 1) % MEM_SIZE];
+        sum += cpu->memory[(champion->program_counter +
+            (first_param % IDX_MOD) + 1) % MEM_SIZE];
     }
+    return sum;
+}
+
+static
+int retrieve_sum(cpu_t *cpu, champions_t *champion, int *bytes, size_t sum)
+{
+    size_t second_param = 0;
+    bool first_register = false;
+
+    sum = update_sum_first(cpu, champion, bytes, &first_register);
     *bytes = retrieve_second_parameter(cpu, champion, &second_param, *bytes);
     if ((first_register == true && *bytes == 3) ||
         (first_register == false && *bytes == 4))
@@ -37,6 +51,24 @@ int retrieve_sum(cpu_t *cpu, champions_t *champion, int *bytes, size_t sum)
     else
         sum += second_param;
     return sum;
+}
+
+static
+void update_register(cpu_t *cpu, champions_t *champion,
+    size_t third_param, size_t sum)
+{
+    champion->registers[third_param] = cpu->memory[
+        (champion->program_counter + (sum % IDX_MOD)) % MEM_SIZE];
+    champion->registers[third_param] <<= 8;
+    champion->registers[third_param] += cpu->memory[
+        (champion->program_counter + (sum % IDX_MOD)) % MEM_SIZE];
+    champion->registers[third_param] <<= 8;
+    champion->registers[third_param] += cpu->memory[
+        (champion->program_counter + (sum % IDX_MOD)) % MEM_SIZE];
+    champion->registers[third_param] <<= 8;
+    champion->registers[third_param] += cpu->memory[
+        (champion->program_counter + (sum % IDX_MOD)) % MEM_SIZE];
+    champion->registers[third_param] <<= 8;
 }
 
 int execute_opcode_ldi(cpu_t *cpu, champions_t *champion)
@@ -50,14 +82,8 @@ int execute_opcode_ldi(cpu_t *cpu, champions_t *champion)
     champion->index = true;
     sum = retrieve_sum(cpu, champion, &bytes, sum);
     bytes = retrieve_third_parameter(cpu, champion, &third_param, bytes);
-    champion->registers[third_param] = cpu->memory[(champion->program_counter + (sum % IDX_MOD)) % MEM_SIZE];
-    champion->registers[third_param] <<= 8;
-    champion->registers[third_param] += cpu->memory[(champion->program_counter + (sum % IDX_MOD)) % MEM_SIZE];
-    champion->registers[third_param] <<= 8;
-    champion->registers[third_param] += cpu->memory[(champion->program_counter + (sum % IDX_MOD)) % MEM_SIZE];
-    champion->registers[third_param] <<= 8;
-    champion->registers[third_param] += cpu->memory[(champion->program_counter + (sum % IDX_MOD)) % MEM_SIZE];
-    champion->registers[third_param] <<= 8;
-    champion->program_counter = (champion->program_counter + bytes + 1) % MEM_SIZE;
+    update_register(cpu, champion, third_param, sum);
+    champion->program_counter = (champion->program_counter + bytes + 1)
+        % MEM_SIZE;
     return SUCCESS;
 }
